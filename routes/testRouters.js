93928886ec8619d0b2626f0f5490add5ec93b9fd
Caren Braj <carenbraj@gmail.com>
2020-12-12 10:15:04 -0800

    api working

--- routes/testRouters.js
+++ routes/testRouters.js
@@ -1,26 +1,35 @@
 const { response } = require('express')
 const express = require('express')
 const axios = require("axios")
+
 const yelp = require('yelp-fusion');
 const client = yelp.client('wz06egUOK6g8xFhV3YrJAgtESWwA-rJnbxG33K1DFEoumLOqCVlDONIrltaG0S0y9brKrerWnlG8-MptZfK54Gd0mUOGsmZO6LX1_FFHKHKz2NzeBAoDgLernuC9X3Yx');
 
-
 const testRouter = express.Router()
-
 
 testRouter.get('/', (req, res) => {
   let params = req.params.path
   let query = req.params.query
   console.log("Hit SERVER ")
-  axios.get("https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972", {'Authorization': "Bearer wz06egUOK6g8xFhV3YrJAgtESWwA-rJnbxG33K1DFEoumLOqCVlDONIrltaG0S0y9brKrerWnlG8-MptZfK54Gd0mUOGsmZO6LX1_FFHKHKz2NzeBAoDgLernuC9X3Yx"}); 
+  // axios.get("https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972", {'Authorization': "Bearer wz06egUOK6g8xFhV3YrJAgtESWwA-rJnbxG33K1DFEoumLOqCVlDONIrltaG0S0y9brKrerWnlG8-MptZfK54Gd0mUOGsmZO6LX1_FFHKHKz2NzeBAoDgLernuC9X3Yx"}) 
+  //   .then(response=> {
+  //     console.log("888888888888")
+  //     console.log("res", response)
+  //     res.json(response)})
+  //   .catch(err=>console.log(err))
 
-    client.allCategories().then(response => {
-      console.log(response.jsonBody.categories[0].alias);
-    }).catch(e => {
-      console.log(e);
-    });
 
 
+client.search({
+  term: 'Four Barrel Coffee',
+  location: 'san francisco, ca',
+}).then(response => {
+  console.log("WE are inside the response")
+  console.log(response.jsonBody);
+  res.json(response.jsonBody)
+}).catch(e => {
+  console.log(e);
+});
   console.log('test working route')
 })
 
