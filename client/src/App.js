a8721af77603a94da0a18bfd023cfd9de23c583a
Raul Oropesa <alexoropesa1982@yahoo.com>
2020-12-15 11:28:57 -0800

    adding login routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
-import { LandingPage } from './LandingPage/LandingPage';
-import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
-import {Search} from './Search/Search';
-import {Reserve} from './reserve/reserve';
+import { LandingPage } from "./LandingPage/LandingPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Search } from "./Search/Search";
+import { Reserve } from "./reserve/reserve";
 import PrivateRoute from "./components/ProtectedHOC/ProtectedRoute";
 import Login from "./pages/Login/Login";
 import CompanyLogos from "./components/CompanyLogos";
@@ -21,17 +21,22 @@ function App() {
             <PrivateRoute path="/pages/Login/Login.js">
               <Login />
             </PrivateRoute>
-
-            <Route exact path="/" component={(props) => <LandingPage {...props} />} />
-            <Route path='/search' component={Search}/>
-            <Route path='/reserve' component={Reserve}/>
+            <Route exact path="/" component={(props) => <Login {...props} />} />
+                        
+            <Route path="/pages/Login/Login.js" comoponent={Login} />
+            <Route path="/search" component={Search} />
+                          
+            <Route exact path="/landingpage" component={LandingPage} />
+                        
+            <LandingPage />
+            <Route path="/reserve" component={Reserve} />
             <Route>
               <h1>404 NOT FOUND</h1>
             </Route>
           </Switch>
-          {/* <CompanyLogos />
+          <CompanyLogos />
           <CovidInfo />
-          <AboutUs /> */}
+          <AboutUs />
         </div>
       </UserProvider>
     </Router>
