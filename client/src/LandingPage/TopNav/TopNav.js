6240048770588d131f4dc88767ad212f96384590
Raul Oropesa <alexoropesa1982@yahoo.com>
2020-12-14 20:41:19 -0800

    updating file

--- client/src/LandingPage/TopNav/TopNav.js
+++ client/src/LandingPage/TopNav/TopNav.js
@@ -4,16 +4,9 @@ import styles from "./TopNav.module.css";
 
 export function TopNav() {
   return (
-    <div className={styles["top-nav"]}>
-      <div className={styles.left}>
-        <span className={styles["nav-link"]}>Write a Review</span>
-        <span className={styles["nav-link"]}>Events</span>
-        <span className={styles["nav-link"]}>Talk</span>
-      </div>
-      {/* <div className={styles.right}>
-        <span className={styles["nav-link"]}>Login</span>
-        <button className={`button ${styles["signup-btn"]}`}>Sign Up</button>
-      </div> */}
+    <div className={styles.right}>
+      <span className={styles["nav-link"]}>Login</span>
+      <button className={`button ${styles["signup-btn"]}`}>Sign Up</button>
     </div>
   );
 }
