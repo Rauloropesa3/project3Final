6c69334441a72825e99dbf4fe67487e94e537440
Carlos Aguilar <carloz0321@gmail.com>
2020-12-14 22:02:14 -0800

    Creating REV page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { LandingPage } from './LandingPage/LandingPage';
 import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
 import {Search} from './Search/Search';
+import {Reserve} from './reserve/reserve';
 import PrivateRoute from "./components/ProtectedHOC/ProtectedRoute";
 import Login from "./pages/Login/Login";
 import CompanyLogos from "./components/CompanyLogos";
@@ -22,6 +23,7 @@ function App() {
 
             <Route exact path="/" component={(props) => <LandingPage {...props} />} />
             <Route path='/search' component={Search}/>
+            <Route path='/reserve' component={Reserve}/>
             <Route>
               <h1>404 NOT FOUND</h1>
             </Route>
