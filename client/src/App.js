9f7ae1f182225eb76d846c0bb584e4588758c279
Raul Oropesa <alexoropesa1982@yahoo.com>
2020-12-14 19:12:30 -0800

    updating file from main

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
-import { LandingPage } from './LandingPage/LandingPage';
-import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
-import {Search} from './Search/Search';
+// import { LandingPage } from "./LandingPage/LandingPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { Search } from "./Search/Search";
 import PrivateRoute from "./components/ProtectedHOC/ProtectedRoute";
 import Login from "./pages/Login/Login";
 import CompanyLogos from "./components/CompanyLogos";
 import CovidInfo from "./components/CovidInfo";
 import AboutUs from "./components/AboutUs";
 import { UserProvider } from "./context/userContext";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { LandingPage } from "./LandingPage/LandingPage";
 
 function App() {
   return (
@@ -16,12 +17,16 @@ function App() {
       <UserProvider>
         <div className="MainBodyContainer">
           <Switch>
-            <PrivateRoute path="/LandingPage">
-              <LandingPage />
+            <PrivateRoute path="/pages/Login/Login.js">
+              <Login />
             </PrivateRoute>
 
-            <Route exact path="/" component={(props) => <LandingPage {...props} />} />
-            <Route path='/search' component={Search}/>
+            <Route exact path="/" component={(props) => <Login {...props} />} />
+            <Route path="/pages/Login/Login.js" comoponent={Login} />
+
+            {/* <Route path="/search" component={Search} /> */}
+            <Route exact path="/landingpage" component={LandingPage} />
+            <LandingPage />
             <Route>
               <h1>404 NOT FOUND</h1>
             </Route>
--- client/src/LandingPage/TopNav/TopNav.js
+++ client/src/LandingPage/TopNav/TopNav.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import styles from './TopNav.module.css';
+import React from "react";
+
+import styles from "./TopNav.module.css";
 
 export function TopNav() {
-    return (
-        <div className={styles['top-nav']}>
-            <div className={styles.right}>
-                <span>Login</span>
-                <button className='button'>Sign up</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles["top-nav"]}>
+      <div className={styles.left}>
+        <span className={styles["nav-link"]}>Write a Review</span>
+        <span className={styles["nav-link"]}>Events</span>
+        <span className={styles["nav-link"]}>Talk</span>
+      </div>
+      {/* <div className={styles.right}>
+        <span className={styles["nav-link"]}>Login</span>
+        <button className={`button ${styles["signup-btn"]}`}>Sign Up</button>
+      </div> */}
+    </div>
+  );
 }
